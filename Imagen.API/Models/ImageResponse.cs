7aabeec06f94e48e0419992098d3218497e92712
Daniel <danielpmacias@gmail.com>
2022-06-05 18:07:52 +0200

    Modelo ImageResponse (JSON)

---
+++ Imagen.API/Models/ImageResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Imagen.API.Models
+{
+    public class ImageResponse
+    {
+        public List<SingleImage> CodedImages { get; set; }
+    }
+
+    public class SingleImage
+    {
+        public string ImageId { get; set; }
+        public byte[] Base64 { get; set; }
+    }
+    
+}
